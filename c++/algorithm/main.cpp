05b018fe56625fd54a78d839167750a52b06bfdf
wenfh2020 <wenfh2020@126.com>
2018-07-27 13:46:38 +0800

    binary sort

--- c++/algorithm/main.cpp
+++ c++/algorithm/main.cpp
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
-//www.algolist.net/Algorithms/Sorting/Quicksort
+void PrintArray(int szArray[], int iBegin, int iEnd) {
+    for (int i = iBegin; i <= iEnd; i++) {
+        printf("%.2d, ", szArray[i]);
+    }
+    printf("\n");
+}
+
 void QuickSort(int szArray[], int iBegin, int iEnd) {
     int iRight = iEnd;
     int iLeft = iBegin;
-    int iFlag = szArray[(iBegin + iEnd) / 2];
+    int iMidValue = szArray[(iBegin + iEnd) / 2];
 
     while (iLeft <= iRight) {
-        while (szArray[iLeft] < iFlag && iLeft < iEnd) {
+        while (szArray[iLeft] < iMidValue && iLeft < iEnd) {
             iLeft++;
         }
 
-        while (szArray[iRight] > iFlag && iRight > iBegin) {
+        while (szArray[iRight] > iMidValue && iRight > iBegin) {
             iRight--;
         }
 
@@ -26,6 +32,9 @@ void QuickSort(int szArray[], int iBegin, int iEnd) {
         }
     };
 
+    printf("mid value = %.2d -- ", iMidValue);
+    PrintArray(szArray, iBegin, iEnd);
+
     if (iBegin < iRight) {
         QuickSort(szArray, iBegin, iRight);
     }
@@ -35,19 +44,32 @@ void QuickSort(int szArray[], int iBegin, int iEnd) {
     }
 }
 
-void PrintArray(int szArray[], int iLen) {
-    for (int i = 0; i < iLen; i++) {
-        printf("%d, ", szArray[i]);
+int BinarySearch(int szArray[], int iSize, int iFindValue) {
+    int iLeft = 0;
+    int iRight = iSize - 1;
+
+    while (iLeft <= iRight) {
+        int iMid = (iLeft + iRight) / 2;
+        if (iFindValue > szArray[iMid]) {
+            iLeft = iMid + 1;
+        } else if (iFindValue < szArray[iMid]) {
+            iRight = iMid - 1;
+        } else {
+            return iMid;
+        }
     }
-    printf("\n");
+
+    return -1;
 }
 
 int main() {
-    int szArray[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    size_t ulSize = sizeof(szArray) / sizeof(int);
-    PrintArray(szArray, ulSize);
+    int szArray[10] = {10, 9, 8, 11, 6, 5, 10, 12, 1, 2};
+    PrintArray(szArray, 0, sizeof(szArray) / sizeof(int) - 1);
+    QuickSort(szArray, 0, sizeof(szArray) / sizeof(int) - 1);
+    PrintArray(szArray, 0, sizeof(szArray) / sizeof(int) - 1);
 
-    QuickSort(szArray, 0, ulSize - 1);
-    PrintArray(szArray, ulSize);
+    int iFindValue = 12;
+    printf("binary search(%d) result %d",
+           iFindValue, BinarySearch(szArray, sizeof(szArray) / sizeof(int), iFindValue));
     return 0;
 }
