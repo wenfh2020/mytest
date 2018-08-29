6df419a48f663cde86dff132d09c734a645eaad8
wenfh2020 <wenfh2020@126.com>
2018-08-29 19:14:53 +0800

    some tcp client test code.

---
+++ c++/pressure/client/client.cpp
@@ -0,0 +1,64 @@
+/* wfh - 2018/8/28
+ * simple socket client.
+ * from: https://blog.csdn.net/u011068702/article/details/56678997
+*/
+
+#include<stdio.h>
+#include <iostream>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<string.h>
+#include<errno.h>
+#include<string.h>
+#include<arpa/inet.h>
+
+int main(int iArgs, char** ppArgv) {
+    if (iArgs != 3) {
+        printf("invalid arg!\n");
+        return 1;
+    }
+
+    int iPort = atoi(ppArgv[2]);
+    std::string strIp(ppArgv[1]);
+
+    int iSockFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == iSockFd) {
+        fprintf(stderr, "Socket Error is %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in tAddr;
+    bzero(&tAddr, sizeof(tAddr));
+    tAddr.sin_family = AF_INET;
+    tAddr.sin_port = htons(iPort);
+    inet_pton(AF_INET, strIp.c_str(), &tAddr.sin_addr.s_addr);
+
+    if (connect(iSockFd, (struct sockaddr*)(&tAddr), sizeof(struct sockaddr)) == -1) {
+        printf("connect failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char szSendBuf[1024] = {0};
+    char szRecvBuf[2014] = {0};
+
+    while (1) {
+        fgets(szSendBuf, sizeof(szSendBuf), stdin);
+        send(iSockFd, szSendBuf, strlen(szSendBuf), 0);
+        if (strcmp(szSendBuf, "exit\n") == 0) {
+            break;
+        }
+
+        recv(iSockFd, szRecvBuf, sizeof(szRecvBuf), 0);
+        fputs(szRecvBuf, stdout);
+        memset(szSendBuf, 0, sizeof(szSendBuf));
+        memset(szRecvBuf, 0, sizeof(szRecvBuf));
+    }
+
+    close(iSockFd);
+    exit(EXIT_SUCCESS);
+    return 0;
+}
---
+++ c++/pressure/client/main.cpp
@@ -0,0 +1,193 @@
+/* wfh - 2018/8/28
+ * epoll for multi clients to pressure.
+ */
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+int SetSocketNoBlock(int iFd) {
+    return fcntl(iFd, F_SETFL, fcntl(iFd, F_GETFL) | O_NONBLOCK);
+}
+
+int EpollAddEvent(int iEpollFd, int iFd) {
+    printf("epoll add event, epoll fd = %d, socket fd = %d\n", iEpollFd, iFd);
+
+    epoll_event tEvent;
+    tEvent.data.fd = iFd;
+    tEvent.events = EPOLLIN | EPOLLET;
+    if (epoll_ctl(iEpollFd, EPOLL_CTL_ADD, iFd, &tEvent) < 0) {
+        printf("epoll %d ctrl add fd %d failed!\n", iEpollFd, iFd);
+        return -1;
+    }
+
+    SetSocketNoBlock(iFd);
+    return 0;
+}
+
+int EpollModEvent(int iEpollFd, int iFd, int iEvent) {
+    printf("epoll mod event, epoll fd: %d, socket fd: %d, event: %d\n",
+           iEpollFd, iFd, iEvent);
+
+    epoll_event tEvent;
+    tEvent.data.fd = iFd;
+    tEvent.events = EPOLLET | EPOLLERR | iEvent;
+    if (epoll_ctl(iEpollFd, EPOLL_CTL_MOD, iFd, &tEvent) < 0) {
+        printf("ctl mod epfd failed! error: %d\n", errno);
+        return -1;
+    }
+
+    SetSocketNoBlock(iFd);
+    return 0;
+}
+
+int EpollDelEvent(int iEpollFd, int iFd) {
+    printf("epoll delete event! epoll fd = %d, socket fd = %d\n", iEpollFd, iFd);
+    return epoll_ctl(iEpollFd, EPOLL_CTL_DEL, iFd, 0);
+}
+
+void CloseConn(int iEpollFd, int iFd) {
+    EpollDelEvent(iEpollFd, iFd);
+    close(iFd);
+}
+
+int WriteData(int iFd, const char* pBuffer, int iLen) {
+    printf("write %d bytes to socket = %d \n", iLen, iFd);
+    if (iFd < 0 || NULL == pBuffer || 0 == iLen) {
+        return -1;
+    }
+
+    int iWriteLen = 0;
+    const char* pBufferIndex = pBuffer;
+
+    while (iLen > 0) {
+        iWriteLen = send(iFd, pBufferIndex, iLen, MSG_NOSIGNAL);
+        if (iWriteLen < 0) {
+            return -1;
+        }
+
+        iLen -= iWriteLen;
+        pBufferIndex += iWriteLen;
+    }
+
+    return iWriteLen;
+}
+
+bool ReadData(int iFd, char* pBuffer, int iLen) {
+    memset(pBuffer, 0, iLen);
+
+    int iReadLen = recv(iFd, pBuffer, iLen, 0);
+    if (iReadLen < 0) {
+        return false;
+    }
+
+    if (0 == iReadLen) {
+        return false;
+    }
+
+    printf("read data from socket = %d, buffer = %s, len = %d\n", iFd, pBuffer, iLen);
+    return true;
+}
+
+void StartConn(int iEpollFd, int iNum, const char* pIP, int iPort) {
+    printf("start conn count = %d, ip = %s, port = %d\n", iNum, pIP, iPort);
+
+    struct sockaddr_in tAddr;
+    bzero(&tAddr, sizeof(tAddr));
+    tAddr.sin_family = AF_INET;
+    tAddr.sin_port = htons(iPort);
+    //tAddr.sin_addr.s_addr = inet_addr(pIP);
+    inet_pton(AF_INET, pIP, &tAddr.sin_addr.s_addr);
+
+    for (int i = 0; i < iNum; i++) {
+        sleep(1);
+
+        int iFd = socket(PF_INET, SOCK_STREAM, 0);
+        if (iFd < 0) {
+            printf("socket error: %d\n", errno);
+            continue;
+        }
+        printf("create socket success, fd = %d\n", iFd);
+
+        if (connect(iFd, (struct sockaddr*)&tAddr, sizeof(tAddr)) < 0) {
+            printf("connect addr faild! \n");
+            continue;
+        }
+
+        printf("create connect success, fd = %d!\n", iFd);
+        EpollAddEvent(iEpollFd, iFd);
+
+        WriteData(iFd, "123", strlen("123"));
+    }
+}
+
+int EpollRun(int iConns, const char* pIP, int iPort) {
+    int iEpollFd = epoll_create(64);
+    if (iEpollFd < 0) {
+        printf("create epoll fd failed! err = %d", errno);
+        return -1;
+    }
+
+    StartConn(iEpollFd, iConns, pIP, iPort);
+
+    char szBuffer[1024] = {0};
+    struct epoll_event arrEvents[10000];
+
+    while (1) {
+        int iEventCount = epoll_wait(iEpollFd, arrEvents, 10000, 2000);
+        for (int i = 0; i < iEventCount; i++) {
+            sleep(1);
+            int iFd = arrEvents[i].data.fd;
+            int iEvents = arrEvents[i].events;
+            if (iFd < 0) {
+                printf("recv data error = %d, fd = %d", errno, iFd);
+                continue;
+            }
+
+            if (iEvents & EPOLLIN) {
+                printf("read data from EPOLLIN\n");
+                if (!ReadData(iFd, szBuffer, sizeof(szBuffer) - 1)) {
+                    CloseConn(iEpollFd, iFd);
+                }
+                EpollModEvent(iEpollFd, iFd, EPOLLOUT);
+            } else if (iEvents & EPOLLOUT) {
+                printf("read data from EPOLLOUT\n");
+                if (WriteData(iFd, "123", strlen("123")) < 0) {
+                    CloseConn(iEpollFd, iFd);
+                }
+                EpollModEvent(iEpollFd, iFd, EPOLLIN);
+            } else if (iEvents & EPOLLERR) {
+                printf("read data from EPOLLERR\n");
+                CloseConn(iEpollFd, iFd);
+            }
+        }
+    }
+
+    return 0;
+}
+
+//./main 100 127.0.0.1 8333
+int main(int iArgs, char** ppArgv) {
+    if (iArgs != 4) {
+        printf("invalid arg! \n");
+        return 1;
+    }
+
+    int iConns = atoi(ppArgv[1]);
+    int iPort = atoi(ppArgv[3]);
+    std::string strIP(ppArgv[2]);
+    printf("conns = % d, ip = %s, port = % d\n", iConns, strIP.c_str(), iPort);
+
+    EpollRun(iConns, strIP.c_str(), iPort);
+    return 0;
+}
